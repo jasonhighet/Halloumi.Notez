3e0cc053f92d7ce98b78c16d7d387a838939591d
Jason Highet <jason.highet@gmail.com>
2019-01-14 09:27:00 +1300

    moved generator to class

---
+++ Halloumi.Notez.Engine/PhraseGenerator.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halloumi.Notez.Engine
+{
+    public class PhraseGenerator
+    {
+        private int _minNotes;
+        private int _maxNotes;
+        private readonly Random _random;
+        private List<NoteProbability> _probabilities;
+        private const int RiffLength = 32;
+
+        public PhraseGenerator()
+        {
+            _random = new Random();
+            LoadTrainingData();
+        }
+
+        private void LoadTrainingData()
+        {
+            var riffs = Directory.GetFiles("TestMidi", "*.mid")
+                .Select(MidiHelper.ReadMidi)
+                .Where(riff => NoteHelper.GetTotalDuration(riff) == RiffLength)
+                .Where(riff => ScaleHelper.FindMatchingScales(riff).Select(x => x.Scale.Name).Contains("C Natural Minor"))
+                .ToList();
+
+            var allNotes = riffs.SelectMany(x => x.Elements).GroupBy(x => new
+                {
+                    x.Position,
+                    x.Note
+                })
+                .Select(x => new
+                {
+                    x.Key.Position,
+                    x.Key.Note,
+                    Count = x.Count()
+                })
+                .OrderBy(x => x.Position)
+                .ThenBy(x => x.Note)
+                .ToList();
+
+            _probabilities = allNotes
+                .Select(x => x.Position)
+                .Distinct()
+                .OrderBy(x => x)
+                .Select(x => new NoteProbability()
+                {
+                    Position = x,
+                    OnOffChance = allNotes.Where(y => y.Position == x).Sum(y => y.Count) / Convert.ToDouble(riffs.Count),
+                    Notes = allNotes.Where(y => y.Position == x).ToDictionary(y => y.Note, y => y.Count)
+                })
+                .ToList();
+
+
+            _minNotes = riffs.Select(x => x.Elements.Count).Min();
+            _maxNotes = riffs.Select(x => x.Elements.Count).Max();
+        }
+
+        private void RemoveNotes(Phrase phrase, int startPosition, int endPosition)
+        {
+            phrase.Elements.RemoveAll(x => x.Position >= startPosition && x.Position <= endPosition);
+        }
+
+        public Phrase GeneratePhrase()
+        {
+            var phrase = new Phrase();
+
+            var noteCount = GetNumberOfNotes();
+
+            var selectedNotes =
+            (from onoffProbability in _probabilities
+                let noteOn = GetRandomBool(onoffProbability.OnOffChance)
+                where noteOn
+                select onoffProbability).ToList();
+
+            while (selectedNotes.Count > noteCount)
+            {
+                var leastPopularNote = selectedNotes.OrderBy(x => x.OnOffChance).FirstOrDefault();
+                selectedNotes.Remove(leastPopularNote);
+            }
+            while (selectedNotes.Count < noteCount)
+            {
+                var mostPopularNote = _probabilities.Except(selectedNotes).OrderByDescending(x => x.OnOffChance).FirstOrDefault();
+                selectedNotes.Add(mostPopularNote);
+            }
+
+            selectedNotes = selectedNotes.OrderBy(x => x.Position).ToList();
+
+            foreach (var note in selectedNotes)
+            {
+                var randomNote = GetRandomNote(note.Notes);
+
+                phrase.Elements.Add(new PhraseElement
+                {
+                    Position = note.Position,
+                    Duration = 1,
+                    Note = randomNote
+                });
+            }
+
+            PhraseHelper.UpdateDurationsFromPositions(phrase, RiffLength);
+
+            return phrase;
+        }
+
+        private int GetRandomNote(Dictionary<int, int> noteNumbers)
+        {
+            var numbers = new List<int>();
+            foreach (var noteNumber in noteNumbers)
+            {
+                for (var i = 0; i < noteNumber.Value; i++)
+                {
+                    numbers.Add(noteNumber.Key);
+                }
+            }
+
+            var randomIndex = _random.Next(0, numbers.Count);
+
+            return numbers[randomIndex];
+        }
+
+        private int GetNumberOfNotes()
+        {
+            var range = _maxNotes - _minNotes;
+            return _minNotes + Convert.ToInt32(Math.Round(range * GetBellCurvedRandom()));
+        }
+
+        private bool GetRandomBool(double chanceOfTrue)
+        {
+            var randomNumber = _random.NextDouble();
+            return randomNumber <= chanceOfTrue;
+        }
+
+        private double GetBellCurvedRandom()
+        {
+            return (Math.Pow(2 * _random.NextDouble() - 1, 3) / 2) + .5;
+        }
+
+        public class NoteProbability
+        {
+            public decimal Position { get; set; }
+
+            public double OnOffChance { get; set; }
+
+            public Dictionary<int, int> Notes { get; set; }
+        }
+    }
+}
--- Halloumi.Notez.TestHarness/Program.cs
+++ Halloumi.Notez.TestHarness/Program.cs
@@ -10,140 +10,15 @@ namespace Halloumi.Notez.TestHarness
     {
         static void Main(string[] args)
         {
-            const int riffLength = 32;
-
-            var riffs = Directory.GetFiles("TestMidi", "*.mid")
-                .Select(MidiHelper.ReadMidi)
-                .Where(riff => NoteHelper.GetTotalDuration(riff) == riffLength)
-                .Where(riff => ScaleHelper.FindMatchingScales(riff).Select(x => x.Scale.Name).Contains("C Natural Minor"))
-                .ToList();
-
-            var allNotes = riffs.SelectMany(x => x.Elements).GroupBy(x => new
-            {
-                x.Position,
-                x.Note
-            })
-            .Select(x => new
-            {
-                x.Key.Position,
-                x.Key.Note,
-                Count = x.Count()
-            })
-            .OrderBy(x => x.Position)
-            .ThenBy(x => x.Note)
-            .ToList();
-
-            var probabilities = allNotes
-                .Select(x => x.Position)
-                .Distinct()
-                .OrderBy(x => x)
-                .Select(x => new
-                {
-                    Position = x,
-                    OnOffChance = allNotes.Where(y => y.Position == x).Sum(y => y.Count) / Convert.ToDouble(riffs.Count),
-                    Notes = allNotes.Where(y => y.Position == x).ToList()
-                })
-                .ToList();
-
-
-            var minNotes = riffs.Select(x => x.Elements.Count).Min();
-            var maxNotes = riffs.Select(x => x.Elements.Count).Max();
-
-            var random = new Random();
-
-            const int riffCount = 32;
+            var generator = new PhraseGenerator();
+            
+            const int riffCount = 8;
             for (var i = 0; i < riffCount; i++)
             {
-                var phrase = new Phrase();
-
-                var noteCount = GetNumberOfNotes(random, minNotes, maxNotes);
-
-                var selectedNotes =
-                (from onoffProbability in probabilities
-                 let noteOn = GetRandomBool(random, onoffProbability.OnOffChance)
-                 where noteOn
-                 select onoffProbability).ToList();
-
-                while (selectedNotes.Count > noteCount)
-                {
-                    var leastPopularNote = selectedNotes.OrderBy(x => x.OnOffChance).FirstOrDefault();
-                    selectedNotes.Remove(leastPopularNote);
-                }
-                while (selectedNotes.Count < noteCount)
-                {
-                    var mostPopularNote = probabilities.Except(selectedNotes).OrderByDescending(x => x.OnOffChance).FirstOrDefault();
-                    selectedNotes.Add(mostPopularNote);
-                }
-
-                selectedNotes = selectedNotes.OrderBy(x => x.Position).ToList();
-
-                foreach (var note in selectedNotes)
-                {
-                    var noteNumbers = note.Notes.ToDictionary(x => x.Note , x => x.Count);
-                    var randomNote = GetRandomNote(random, noteNumbers);
-                    
-                    phrase.Elements.Add(new PhraseElement
-                    {
-                        Position = note.Position,
-                        Duration = 1,
-                        Note = randomNote
-                    });
-                }
-
-                PhraseHelper.UpdateDurationsFromPositions(phrase, riffLength);
-
+                var phrase = generator.GeneratePhrase();
                 MidiHelper.SaveToMidi(phrase, "Riff" + i + ".mid");
             }
-
-
-
-            Console.WriteLine();
-
-
-
-
-           // Console.ReadLine();
+            // Console.ReadLine();
         }
-
-        private static int GetRandomNote(Random random, Dictionary<int, int> noteNumbers)
-        {
-            var numbers = new List<int>();
-            foreach (var noteNumber in noteNumbers)
-            {
-                for (var i = 0; i < noteNumber.Value; i++)
-                {
-                    numbers.Add(noteNumber.Key);
-                }
-            }
-
-            var randomIndex = random.Next(0, numbers.Count);
-
-            return numbers[randomIndex];
-        }
-
-        private static int GetNumberOfNotes(Random random, int minNotes, int maxNotes)
-        {
-            var range = maxNotes - minNotes;
-            //return minNotes + Convert.ToInt32(Math.Round(range * GetBellCurvedRandom(random)));
-            return minNotes + Convert.ToInt32(Math.Round(range * random.NextDouble()));
-        }
-
-        private static bool GetRandomBool(Random random, double chanceOfTrue)
-        {
-            var randomNumber = random.NextDouble();
-            return randomNumber <= chanceOfTrue;
-        }
-
-        private static double GetBellCurvedRandom(Random random)
-        {
-            return (Math.Pow(2 * random.NextDouble() - 1, 3) / 2) + .5;
-        }
-
-        //public class NoteProbability
-        //{
-        //    public decimal Position { get; set; }
-
-        //    public double Chance { get; set; }
-        //}
     }
 }
