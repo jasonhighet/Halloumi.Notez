537b5f988e3941c196202d93a45eeee81131408f
Jason Highet <jason.highet@gmail.com>
2019-04-04 20:02:22 +1300

    notez

--- Halloumi.Notez.TestHarness/Program.cs
+++ Halloumi.Notez.TestHarness/Program.cs
@@ -11,20 +11,45 @@ namespace Halloumi.Notez.TestHarness
         static void Main(string[] args)
         {
             const int riffCount = 24;
+
+            DeleteMidiFiles();
+
             var generator = new PhraseGenerator(32, @"Doom\Main");
             for (var i = 1; i <= riffCount; i++)
             {
-                var midiPath = @"RandomRiff" + i + ".mid";
-                var phrase = generator.GeneratePhrase();
 
-                phrase = NoteHelper.ShiftNotes(phrase, 1, Interval.Octave, Direction.Down);
-                PhraseHelper.DuplicatePhrase(phrase);
-                PhraseHelper.DuplicatePhrase(phrase);
+                var phrase = generator.GeneratePhrase();
+                string midiPath = GetFileName(i, phrase);
 
                 MidiHelper.SaveToMidi(phrase, midiPath, MidiInstrument.OverdrivenGuitar);
             }
 
             //Console.ReadLine();
+        }
+
+        private static string GetFileName(int index, Phrase phrase)
+        {
+            var lowNote = phrase.Elements.Min(x => x.Note);
+            var getUpCount = GetUpCount(lowNote);
+            var midiPath = @"RandomRiff" + index + "_" + getUpCount + ".mid";
+            return midiPath;
+        }
+
+        private static void DeleteMidiFiles()
+        {
+            foreach (string file in Directory.EnumerateFiles(".", "*.mid"))
+            {
+                File.Delete(file);
+            }
+        }
+
+        private static int GetUpCount(int lowNote)
+        {
+            var noteName = NoteHelper.NumberToNote(lowNote);
+            if (noteName == "B2" || noteName == "C3")
+                return 0;
+
+            return 35 - lowNote;
         }
     }
 }
