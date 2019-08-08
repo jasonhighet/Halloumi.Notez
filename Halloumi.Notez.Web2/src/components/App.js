4892df148c43c46bbeafee4d90905eb2daad1c96
Jason Highet <jason.highet@gmail.com>
2019-08-08 13:01:51 +1200

    notez

--- Halloumi.Notez.Web2/src/components/App.js
+++ Halloumi.Notez.Web2/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import MIDI from "midicube";
+import MidiPlayer from "midi-player-js";
+import Soundfont from "soundfont-player";
 
 class App extends Component {
   constructor(props) {
@@ -10,13 +11,50 @@ class App extends Component {
 
     };
 
+    this.audioContext = new AudioContext();
+
+    Soundfont.instrument(this.audioContext, 'clavinet').then(instrument=>{
+        this.instrument = instrument;
+    });
+
     this.init();
   }
 
-  
 
+  async loadMidiFromUri(midiUri) {
+    return new Promise((resolve, reject) => {
+      var request = new XMLHttpRequest();
+      request.open('GET', midiUri, true);
+      request.responseType = 'blob';
+      request.onload = function() {
+          var reader = new FileReader();
+          reader.readAsDataURL(request.response);
+          reader.onload = function(e){
+              resolve(e.target.result);
+          };
+          reader.onerror = function(error) {
+            reject(error);
+        };
+      };
+      request.send();
+    });
+    
+  }
+  
   async init() {
-    this.player = MIDI.player();
+
+    // // Initialize player and register event handler
+    var player = new MidiPlayer.Player(event => {
+         //console.log(event);
+         if (event.name === 'Note on' && event.velocity > 0) {
+          this.instrument.play(event.noteName, this.audioContext.currentTime, {gain:event.velocity/100});
+        }
+    });
+
+    this.loadMidiFromUri("https://bitmidi.com/uploads/105027.mid").then(midi => {
+      player.loadDataUri(midi);
+      player.play();
+    });
   }
 
 
