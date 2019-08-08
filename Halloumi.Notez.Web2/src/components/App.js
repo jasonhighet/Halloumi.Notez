2cdc781b786e0c578cedbf6ff0f3e0c8a8c50390
Jason Highet <jason.highet@gmail.com>
2019-08-08 15:34:29 +1200

    clips

--- Halloumi.Notez.Web2/src/components/App.js
+++ Halloumi.Notez.Web2/src/components/App.js
@@ -2,68 +2,85 @@ import React, { Component } from "react";
 import Header from "./Header";
 import MidiPlayer from "midi-player-js";
 import Soundfont from "soundfont-player";
+import MidiParser from "midi-parser-js";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-
-    };
+    this.state = {};
 
     this.audioContext = new AudioContext();
 
-    Soundfont.instrument(this.audioContext, 'clavinet').then(instrument=>{
-        this.instrument = instrument;
+    Soundfont.instrument(this.audioContext, "clavinet").then(instrument => {
+      this.instrument = instrument;
     });
 
     this.init();
   }
 
-
   async loadMidiFromUri(midiUri) {
     return new Promise((resolve, reject) => {
       var request = new XMLHttpRequest();
-      request.open('GET', midiUri, true);
-      request.responseType = 'blob';
+      request.open("GET", midiUri, true);
+      request.responseType = "blob";
       request.onload = function() {
-          var reader = new FileReader();
-          reader.readAsDataURL(request.response);
-          reader.onload = function(e){
-              resolve(e.target.result);
-          };
-          reader.onerror = function(error) {
-            reject(error);
+        var reader = new FileReader();
+        reader.readAsDataURL(request.response);
+        reader.onload = function(e) {
+          resolve(e.target.result);
+        };
+        reader.onerror = function(error) {
+          reject(error);
         };
       };
       request.send();
     });
-    
   }
-  
-  async init() {
 
+  async init() {
     // // Initialize player and register event handler
     var player = new MidiPlayer.Player(event => {
-         //console.log(event);
-         if (event.name === 'Note on' && event.velocity > 0) {
-          this.instrument.play(event.noteName, this.audioContext.currentTime, {gain:event.velocity/100});
-        }
+      console.log(event);
+      if (event.name === "Note on" && event.velocity > 0) {
+        this.instrument.play(event.noteName, this.audioContext.currentTime, {
+          gain: event.velocity / 100
+        });
+      }
     });
 
-    this.loadMidiFromUri("https://bitmidi.com/uploads/105027.mid").then(midi => {
+    this.loadMidiFromUri("http://localhost:9000/api/randomriff").then(midi => {
       player.loadDataUri(midi);
       player.play();
+
+      //   var midiData = MidiParser.parse(midi);
+      //   var tracks = [];
+      //   midiData.track.forEach((track, trackIndex) => {
+      //     var trackData = {
+      //       index: trackIndex,
+      //       isDrums: track.event.find(event => {
+      //         return event.channel === 9;
+      //       })
+      //         ? true
+      //         : false
+      //     };
+      //     var programChange = track.event.find(event => {
+      //       return event.type === 12;
+      //     });
+
+      //     if (trackData.isDrums || programChange) {
+      //       trackData.instrument = programChange ? programChange.data : 0;
+      //       console.log(trackData);
+      //       tracks.push(trackData);
+      //     }
+      //   });
     });
   }
-
 
   render() {
     return (
       <>
-        <Header >
-          
-        </Header>
+        <Header />
       </>
     );
   }
