e8980a938133ef02034c3f80adf30bc70165115c
Jason Highet <jason.highet@gmail.com>
2019-08-12 13:33:30 +1200

    Update App.js

--- Halloumi.Notez.Web2/src/components/App.js
+++ Halloumi.Notez.Web2/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./Header";
 import MidiPlayer from "midi-player-js";
 import Soundfont from "soundfont-player";
+import { Button } from "react-bootstrap";
 import MidiParser from "midi-parser-js";
 
 class App extends Component {
@@ -9,13 +10,12 @@ class App extends Component {
     super(props);
 
     this.state = {};
+    this.instruments = {};
+    this.tracks = [];
+    this.player = null;
+  }
 
-    this.audioContext = new AudioContext();
-
-    Soundfont.instrument(this.audioContext, "clavinet").then(instrument => {
-      this.instrument = instrument;
-    });
-
+  handleAddClick() {
     this.init();
   }
 
@@ -39,41 +39,79 @@ class App extends Component {
   }
 
   async init() {
+    if (!this.audioContext) this.audioContext = new AudioContext();
+
+    if (!this.instruments[1])
+      await Soundfont.instrument(this.audioContext, "overdriven_guitar").then(
+        instrument => {
+          this.instruments[1] = instrument;
+          console.log(instrument);
+        }
+      );
+
+    if (!this.instruments[2])
+      await Soundfont.instrument(this.audioContext, "distortion_guitar").then(
+        instrument => {
+          this.instruments[2] = instrument;
+          console.log(instrument);
+        }
+      );
+
+    if (!this.instruments[3])
+      await Soundfont.instrument(
+        this.audioContext,
+        "electric_bass_finger"
+      ).then(instrument => {
+        this.instruments[3] = instrument;
+        console.log(instrument);
+      });
+
     // // Initialize player and register event handler
-    var player = new MidiPlayer.Player(event => {
-      console.log(event);
-      if (event.name === "Note on" && event.velocity > 0) {
-        this.instrument.play(event.noteName, this.audioContext.currentTime, {
-          gain: event.velocity / 100
-        });
-      }
-    });
+    if (!this.player)
+      this.player = new MidiPlayer.Player(event => {
+        console.log(event);
+        if (!this.instruments[event.track]) return;
+
+        if (event.name === "Note on") {
+          this.instruments[event.track].play(
+            event.noteName,
+            this.audioContext.currentTime,
+            {
+              gain: event.velocity / 100
+            }
+          );
+        }
+
+        if (event.name === "Note off") {
+          this.instruments[event.track].stop();
+        }
+      });
 
     this.loadMidiFromUri("http://localhost:9000/api/randomriff").then(midi => {
-      player.loadDataUri(midi);
-      player.play();
+      var midiData = MidiParser.parse(midi);
+      midiData.track.forEach((track, trackIndex) => {
+        var trackData = {
+          index: trackIndex,
+          isDrums: track.event.find(event => {
+            return event.channel === 9;
+          })
+            ? true
+            : false
+        };
+        var programChange = track.event.find(event => {
+          return event.type === 12;
+        });
 
-      //   var midiData = MidiParser.parse(midi);
-      //   var tracks = [];
-      //   midiData.track.forEach((track, trackIndex) => {
-      //     var trackData = {
-      //       index: trackIndex,
-      //       isDrums: track.event.find(event => {
-      //         return event.channel === 9;
-      //       })
-      //         ? true
-      //         : false
-      //     };
-      //     var programChange = track.event.find(event => {
-      //       return event.type === 12;
-      //     });
+        if (trackData.isDrums || programChange) {
+          trackData.instrumentNumber = programChange ? programChange.data : 0;
+          console.log(trackData);
+          this.tracks.push(trackData);
+        }
+      });
 
-      //     if (trackData.isDrums || programChange) {
-      //       trackData.instrument = programChange ? programChange.data : 0;
-      //       console.log(trackData);
-      //       tracks.push(trackData);
-      //     }
-      //   });
+      this.player.stop();
+      this.player.loadDataUri(midi);
+      this.player.play();
     });
   }
 
@@ -81,6 +119,7 @@ class App extends Component {
     return (
       <>
         <Header />
+        <Button onClick={this.handleAddClick.bind(this)}>Play</Button>
       </>
     );
   }
