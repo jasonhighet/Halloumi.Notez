aa27673927cfdc417434bc64b4391c5ca52e43de
Jason Highet <jason.highet@gmail.com>
2019-08-18 21:26:37 +1200

    midi

--- Halloumi.Notez.TestHarness/Program.cs
+++ Halloumi.Notez.TestHarness/Program.cs
@@ -16,58 +16,62 @@ namespace Halloumi.Notez.TestHarness
     {
         static void Main(string[] args)
         {
-            var folder = @"..\..\..\Halloumi.Notez.Engine\TestMidi\Doom\";
-            var drums = MidiHelper.ReadMidi(folder + "drums.mid");
-            foreach (string midiFile in Directory.EnumerateFiles(".", "*.mid")) File.Delete(midiFile);
+            //MidiFileLibraryHelper.CopyPlaylistFiles(@"E:\OneDrive\Music\Midi\metal\doom.mpl", @"C:\Users\jason\Desktop\metalmidi\doom");
 
-            foreach (string midiFile in Directory.EnumerateFiles(folder, "*.mid", SearchOption.AllDirectories))
-            {
-                if (Path.GetFileName(midiFile) == "drums.mid")
-                    continue;
 
-                Console.WriteLine(midiFile);
+            //var folder = @"..\..\..\Halloumi.Notez.Engine\TestMidi\Doom\";
+            //var drums = MidiHelper.ReadMidi(folder + "drums.mid");
+            //foreach (string midiFile in Directory.EnumerateFiles(".", "*.mid")) File.Delete(midiFile);
 
-                var section = MidiHelper.ReadMidi(midiFile);
-                section.Phrases[0].Bpm = 70;
-                section.Phrases[0].Instrument = MidiInstrument.DistortedGuitar;
-
-                section.Phrases.Add(section.Phrases[0].Clone());
-                section.Phrases[1].Instrument = MidiInstrument.OverdrivenGuitar;
-
-                var bassPhrase = section.Phrases[0].Clone();
-                NoteHelper.ShiftNotesDirect(bassPhrase, 1, Interval.Octave, Direction.Down);
-                bassPhrase.Instrument = MidiInstrument.ElectricBassPick;
-                section.Phrases.Add(bassPhrase);
-
-                var drumPhrase = drums.Phrases[0].Clone();
-                drumPhrase.IsDrums = true;
-
-                section.Phrases.Add(drumPhrase);
-
-                for (int i = 0; i < 3; i++)
-                {
-                    NoteHelper.ShiftNotesDirect(section.Phrases[i], 2, Interval.Octave, Direction.Down);
-                    VelocityHelper.ApplyVelocityStrategy(section.Phrases[i], "FlatHigh");
-                }
-                PhraseHelper.EnsureLengthsAreEqual(section.Phrases);
-
-                MidiHelper.SaveToMidi(section, Path.GetFileName(midiFile));
-            }
-            Console.ReadLine();
-
-            //var sourceLibrary = new SectionGenerator(folder);
-
-            //while (true)
+            //foreach (string midiFile in Directory.EnumerateFiles(folder, "*.mid", SearchOption.AllDirectories))
             //{
-            //    foreach (string midiFile in Directory.EnumerateFiles(".", "*.mid")) File.Delete(midiFile);
-            //    var now = DateTime.Now.ToString("yyyymmddhhss");
+            //    if (Path.GetFileName(midiFile) == "drums.mid")
+            //        continue;
 
-            //    //sourceLibrary.MergeSourceClips();
-            //    sourceLibrary.GenerateRiffs(now, 20);
+            //    Console.WriteLine(midiFile);
 
-            //    Console.WriteLine("push any key..");
-            //    Console.ReadLine();
+            //    var section = MidiHelper.ReadMidi(midiFile);
+            //    section.Phrases[0].Bpm = 70;
+            //    section.Phrases[0].Instrument = MidiInstrument.DistortedGuitar;
+
+            //    section.Phrases.Add(section.Phrases[0].Clone());
+            //    section.Phrases[1].Instrument = MidiInstrument.OverdrivenGuitar;
+
+            //    var bassPhrase = section.Phrases[0].Clone();
+            //    NoteHelper.ShiftNotesDirect(bassPhrase, 1, Interval.Octave, Direction.Down);
+            //    bassPhrase.Instrument = MidiInstrument.ElectricBassPick;
+            //    section.Phrases.Add(bassPhrase);
+
+            //    var drumPhrase = drums.Phrases[0].Clone();
+            //    drumPhrase.IsDrums = true;
+
+            //    section.Phrases.Add(drumPhrase);
+
+            //    for (int i = 0; i < 3; i++)
+            //    {
+            //        NoteHelper.ShiftNotesDirect(section.Phrases[i], 2, Interval.Octave, Direction.Down);
+            //        VelocityHelper.ApplyVelocityStrategy(section.Phrases[i], "FlatHigh");
+            //    }
+            //    PhraseHelper.EnsureLengthsAreEqual(section.Phrases);
+
+            //    MidiHelper.SaveToMidi(section, Path.GetFileName(midiFile));
             //}
+            //Console.ReadLine();
+
+            var folder = @"..\..\..\Halloumi.Notez.Engine\TestMidi\Death\";
+            var sourceLibrary = new SectionGenerator(folder);
+
+            while (true)
+            {
+                foreach (string midiFile in Directory.EnumerateFiles(".", "*.mid")) File.Delete(midiFile);
+                var now = DateTime.Now.ToString("yyyymmddhhss");
+
+                sourceLibrary.MergeSourceClips();
+                sourceLibrary.GenerateRiffs(now, 20);
+
+                Console.WriteLine("push any key..");
+                Console.ReadLine();
+            }
 
         }
 
