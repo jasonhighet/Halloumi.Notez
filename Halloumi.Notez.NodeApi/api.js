497a1e42c3c269d023553c2999cbe41cd52fbc90
Jason Highet <jason.highet@gmail.com>
2020-01-28 17:35:18 +1300

    Update node API

--- Halloumi.Notez.NodeApi/api.js
+++ Halloumi.Notez.NodeApi/api.js
@@ -3,10 +3,14 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
 
-const mode = require("@magenta/music/node/music_vae");
+const music_vae = require("@magenta/music/node/music_vae");
+const music_rnn = require("@magenta/music/node/music_rnn");
 const core = require("@magenta/music/node/core");
+
 const VAE_CHECKPOINTS =
   "https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2";
+const RNN_CHECKPOINTS =
+  "https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn";
 
 // // These hacks below are needed because the library uses performance and fetch which
 // // exist in browsers but not in node. We are working on simplifying this!
@@ -14,8 +18,11 @@ var globalAny = global;
 globalAny.performance = Date;
 globalAny.fetch = require("node-fetch");
 
-const model = new mode.MusicVAE(VAE_CHECKPOINTS);
-model.initialize();
+const musicVAE = new music_vae.MusicVAE(VAE_CHECKPOINTS);
+musicVAE.initialize();
+
+const musicRNN = new music_rnn.MusicRNN(RNN_CHECKPOINTS);
+//musicRNN.initialize();
 
 const app = express();
 const port = 3000;
@@ -27,15 +34,33 @@ app.use(bodyParser.json());
 var upload = multer();
 
 app.post("", upload.array("midi"), (req, res) => {
-  console.log(req.files[0]);
   var file = req.files[0];
 
-  res.writeHead(200, {
-    "Content-Type": file.mimetype,
-    "Content-disposition": "attachment;filename=" + file.originalname,
-    "Content-Length": file.size
-  });
-  res.end(file.buffer);
+  var midi1 = req.files[0].buffer.toString("binary");
+  var sequence1 = core.midiToSequenceProto(midi1);
+  sequence1 = core.sequences.quantizeNoteSequence(sequence1, 8);
+
+  var midi2 = req.files[1].buffer.toString("binary");
+  var sequence2 = core.midiToSequenceProto(midi2);
+  sequence2 = core.sequences.quantizeNoteSequence(sequence2, 8);
+
+  var numInterpolations = 3;
+  musicVAE
+    .interpolate([sequence1, sequence2], numInterpolations)
+    .then(interpolations => {
+      var newSequence = interpolations[1];
+
+      var newMidi = core.sequenceProtoToMidi(newSequence);
+
+      var buffer = Buffer.from(newMidi, "binary");
+
+      res.writeHead(200, {
+        "Content-Type": file.mimetype,
+        "Content-disposition": "attachment;filename=" + file.originalname,
+        "Content-Length": buffer.length
+      });
+      res.end(buffer);
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
